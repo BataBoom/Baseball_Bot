e68cf33ef6daa46b82de533a95ae836eae3d0465
BB <contact@bataboom.dev>
2024-07-11 17:05:04 -0500

    add mlb lang file

---
+++ lang/en/mlb.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+'game' => ['homeTeam' => ":value", 'awayTeam' => ":value"],
+'description' =>  ":value",
+"pitchingHome" => ":value",
+"pitchingAway" => ":value",
+"scoreHome" => ":value",
+"scoreAway" => ":value",
+"errorsHome" => ":value",
+"errorsAway" => ":value",
+"abrHome" => ":value",
+"abrAway" => ":value",
+"Weather" => ":value",
+"Status" => ":value"
+];
